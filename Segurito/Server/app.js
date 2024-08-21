b4ba299e9ef38b22073207ad020e8e96864d41a2
holamundo-ui <ezequielalvares466@gmail.com>
2024-08-21 12:32:52 -0300

    Delete Segurito/Server directory

--- Segurito/Server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('../')); // Sirve los archivos estáticos
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
