a78b3cdf9b73af5362622fa1ed67fdd76a0175c4
holamundo-ui <ezequielalvares466@gmail.com>
2024-12-28 20:58:44 -0300

    Delete jiji/Log.js

--- jiji/Log.js
+++
@@ -1,9 +0,0 @@
-console.log("Iniciando el script...");
-
-(async () => {
-    try {
-        console.log("Esto es un mensaje de prueba");
-    } catch (error) {
-        console.error("Error encontrado:", error);
-    }
-})();
